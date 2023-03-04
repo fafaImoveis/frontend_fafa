2abb3ab9866bb4ca3df77a9c3601d3a4f66a1b67
fafaImoveis <126974250+fafaImoveis@users.noreply.github.com>
2023-03-04 15:24:45 -0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ export const Footer = () => {
 
                 </div>
                 <div className="contato">
-                    <h1>Contato</h1>
+                    <h2>Contato</h2>
                     <p><a href="https://api.whatsapp.com/send?phone=551155266144&text=Olá, Eu gostaria de anunciar meu imóvel!!">(11) 5526-6144</a></p>
                     <p>      R. Rocha de magalhães, n° 08 <br></br> <br></br> Próximo a Av. Senador teotônio villela <br></br> ao lado da estação Term. Varginha - sp</p>
                 </div>
