d8e503013a0ecf64e38c3bcfc2a144133486e74f
fafaImoveis <126974250+fafaImoveis@users.noreply.github.com>
2023-05-13 13:37:29 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiu = ''
 
 const api = axios.create({
-    baseURL: "http://18.204.16.50:3333",
+    baseURL: "https://www.fagundesimoveis.app.br",
 });
 
 export default api;
