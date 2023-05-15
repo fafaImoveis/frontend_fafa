d7faa78f3485ecd38208bc78ab16a042c0c9bb68
fafaImoveis <126974250+fafaImoveis@users.noreply.github.com>
2023-05-15 16:28:14 -0300

    Update Banks.jsx

--- src/components/simulation/Banks.jsx
+++ src/components/simulation/Banks.jsx
@@ -28,6 +28,7 @@ export const Banks = () => {
             <Simulation>
                 <h2>ESCOLHA SEU BANCO E SIMULE UM FINANCIAMENTO</h2>
                <Carousel responsive={responsive} className="carousel">
+                    <a href="https://ww3.itau.com.br/imobline/novolayout/simuladores/simulador.aspx?IMOB_TipoBKL=&ident_bkl=pre&parceiro=0835"><div className="bank03"><div className="photo"></div><p>Caixa</p></div></a> 
                    <a href="https://www.santander.com.br/creditos-e-financiamentos/para-sua-casa/credito-imobiliario?ic=homepf-cardsprod-creditoimobiliario#/dados-pessoais"><div className="bank04"><div className="photo"></div><p>Santander</p></div></a> 
                    <a href="https://banco.bradesco/html/classic/produtos-servicos/emprestimo-e-financiamento/encontre-seu-credito/simuladores-imoveis.shtm#box1-comprar"><div className="bank01"><div className="photo"></div><p>Bradesco</p></div></a>
                    <a href="https://www42.bb.com.br/portalbb/imobiliario/creditoimobiliario/simular,802,2250,2250.bbx?eni_gclid=Cj0KCQjwp86EBhD7ARIsAFkgakg39StNF0YBE3S5bgNiBlnuM-BMJd6hXU5ACfQtaweeFYraleQ5fqUaAlvvEALw_wcB&pk_vid=e096ac2feeed30bf162033156524f1ab"><div className="bank02"><div className="photo"></div><p>Banco do Brasil</p></div></a>
